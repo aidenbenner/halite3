9efabfede9cb3da2c95b479fdcc69f7c613c3ca8
Aiden Benner <aiden.benner@gmail.com>
2018-10-18 00:30:12 -0400

    Initial commit

---
+++ CMakeFiles/MyBot.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyBot.dir/hlt/command.cpp.o"
+  "CMakeFiles/MyBot.dir/hlt/constants.cpp.o"
+  "CMakeFiles/MyBot.dir/hlt/dropoff.cpp.o"
+  "CMakeFiles/MyBot.dir/hlt/game.cpp.o"
+  "CMakeFiles/MyBot.dir/hlt/game_map.cpp.o"
+  "CMakeFiles/MyBot.dir/hlt/log.cpp.o"
+  "CMakeFiles/MyBot.dir/hlt/player.cpp.o"
+  "CMakeFiles/MyBot.dir/hlt/ship.cpp.o"
+  "CMakeFiles/MyBot.dir/MyBot.cpp.o"
+  "MyBot.pdb"
+  "MyBot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyBot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
