7f5d4ff7de98288f1d11d7139e9ca299a0e7de1a
Aiden Benner <aiden.benner@gmail.com>
2018-11-16 19:55:59 -0500

    improve upload

--- util/upload.py
+++ util/upload.py
@@ -2,12 +2,23 @@
 
 import os
 
-print('which version is this?')
+print('Version #')
 version = int(input())
+
+result = os.system('make')
+if result != 0:
+    print("Did not compile")
+    sys.exit()
 
 os.system('zip MyBot.zip MyBot.cpp hlt/* CMakeLists.txt cmake_install.cmake Makefile CMakeFiles')
 os.system('hlt bot -b ./MyBot.zip upload')
 os.system('cp MyBot ./bots/' + str(version))
-os.system('hlt gym register %s ./bots/%s' % (version, version))
+os.system('hlt gym register %s ./bots/v%s' % (version, version))
 
-print('remember to commit')
+print('Did you commit? y/n')
+response = input()
+
+if response == 'y':
+    os.system('git tag -a v%s' %s version)
+else:
+    print("please tag the branch")
